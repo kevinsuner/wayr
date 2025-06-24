f793c84efdfa98e7afac02eca7a85c92c843a652
Kevin Suñer <kevinsuner99@gmail.com>
2025-06-24 22:06:15 +0200

    Add functions to display duration and interval value, update element IDs

--- main.js
+++ main.js
@@ -15,12 +15,26 @@ function speak(text) {
 }
 
 /**
+ * Displays the value of `#duration-range` slider.
+ */
+function set_duration() {
+  document.getElementById("duration").innerHTML = document.getElementById("duration-range").value;
+}
+
+/**
+ * Displays the value of `#interval-range` slider.
+ */
+function set_interval() {
+  document.getElementById("interval").innerHTML = document.getElementById("interval-range").value;
+}
+
+/**
  * Triggers `speak` recurrently informing about the finished interval and the
  * remaining duration.
  */
 function start() {
-  let duration = document.getElementById("duration-input").value;
-  let interval = document.getElementById("interval-input").value;
+  let duration = document.getElementById("duration-range").value;
+  let interval = document.getElementById("interval-range").value;
   const now = new Date();
   const end = new Date(now.getTime() + duration * 60000); // N minutes * 60 seconds * 1000 miliseconds
   
