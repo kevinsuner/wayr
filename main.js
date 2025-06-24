c535a13e83e322a30bef165385da66239cdd0321
Kevin Suñer <kevinsuner99@gmail.com>
2025-06-24 18:49:15 +0200

    Triggering text-to-speech based on a duration and an interval

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const voice_name = "Mónica";
-const voice_speed = 0.85;
+const VOICE_NAME = "Mónica";
+const VOICE_SPEED = 0.85;
 
 /**
  * Text to speech using the Web Speech API.
@@ -8,8 +8,26 @@ const voice_speed = 0.85;
 function speak(text) {
   const utterance = new SpeechSynthesisUtterance(text);
   const voices = speechSynthesis.getVoices();
-  const voice_index = voices.findIndex((element) => element.name === voice_name);
+  const voice_index = voices.findIndex((element) => element.name === VOICE_NAME);
   utterance.voice = voices[voice_index];
-  utterance.rate = voice_speed;
+  utterance.rate = VOICE_SPEED;
   speechSynthesis.speak(utterance);
+}
+
+/**
+ * Triggers `speak` recurrently informing about the finished interval and the
+ * remaining duration.
+ */
+function start() {
+  let duration = document.getElementById("duration-input").value;
+  let interval = document.getElementById("interval-input").value;
+  const now = new Date();
+  const end = new Date(now.getTime() + duration * 60000); // N minutes * 60 seconds * 1000 miliseconds
+  
+  setInterval(() => {
+    if (now.getTime() < end) {
+      duration -= interval;
+      speak(`Intervalo de ${interval} minutos completado, ${duration} minutos restantes`);
+    }
+  }, interval * 60000);
 }
